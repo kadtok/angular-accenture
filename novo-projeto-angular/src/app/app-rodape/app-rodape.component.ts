b56ce5f1eeea2d285a4d0d8c8079511a9d5483bf
Karen <kadtok@gmail.com>
2018-04-18 09:48:29 -0300

    Componentes

---
+++ novo-projeto-angular/src/app/app-rodape/app-rodape.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-app-rodape',
+  templateUrl: './app-rodape.component.html',
+  styleUrls: ['./app-rodape.component.css']
+})
+export class AppRodapeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- novo-projeto-angular/src/app/topo/topo.component.ts
+++ novo-projeto-angular/src/app/topo/topo.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component ({
     selector: 'app-topo',
     templateUrl: './topo.component.html',
-    styleUrls:['node_modules/bootstrap/dist/css/']
+    styleUrls:['./topo.component.css']
 })
 export class TopoApp{
 
